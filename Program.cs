2d8b810ea6d0431cbab73d15887568e8c556e706
caravena <caravena@exisoft.com.ar>
2023-02-25 00:45:08 -0300

    add conecctionString exitoso, creación de TareasDB y Tablas

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+//Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;user Id=turko;password=superman17;
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
-
+builder.Services.AddSqlServer<TareasContext>("Data Source=E000158\\SQLEXPRESS;Database=master;Initial Catalog=TareasDb;Trust Server Certificate=true;Trusted_Connection=Yes");
 
 
 var app = builder.Build();
@@ -15,7 +16,7 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/dbConexion", async ([FromServices] TareasContext dbContext) => 
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.CanConnect());
 });
 
 app.Run();
