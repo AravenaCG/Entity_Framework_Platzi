3412e56b092464aa31d98761584c6f3579ffd1c2
caravena <caravena@exisoft.com.ar>
2023-02-24 23:21:56 -0300

    configuracion de database in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Entity_Framework_Platzi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConexion", async ([FromServices] TareasContext dbContext) => 
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
